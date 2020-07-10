3c704f41f175a74f508448f540ee6c0eb3ace053
sshr0053 <sshr0053@yahoo.com>
2020-07-10 12:06:01 -0600

    portfolio-container.js and poerfolio items

---
+++ src/components/portfolio/portfolio-container.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+
+import PortfolioItem from './portfolio-item';
+
+export default class PortfolioContainer extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            pageTitle: "Welcome to my Portfolio",
+            data: [
+                {title: "Quip", category: "project" }, 
+                {title: "Eventbrite", category: "Scheduling" }, 
+                {title:"Ministry safe", category: "Enterprise"},
+                {title: "SwingAway", category: "project" }
+            ] 
+        };
+        
+        this.handleFilter = this.handleFilter.bind(this);
+    }
+
+    handleFilter(filter) {
+        this.setState({
+            data: this.state.data.filter(item => {
+                return item.category === filter;
+            })
+        });
+    }
+
+    portfolioItems() {
+        return this.state.data.map(item => {
+            return <PortfolioItem title={item.title} url={"google.com"} />;
+        });
+    }
+
+
+    render() {
+        return (
+            <div>
+                <h2>{this.state.pageTitle}</h2>
+
+                <button onClick={() => this.handleFilter('project')}>
+                    Project
+                </button>
+                <button onClick={() => this.handleFilter('scheduling')}>
+                    Scheduling
+                </button>
+                <button onClick={() => this.handleFilter('Enterprise')}>
+                    Enterprise
+                </button>
+
+                {this.portfolioItems()}
+
+            </div>
+        );
+    }
+}
