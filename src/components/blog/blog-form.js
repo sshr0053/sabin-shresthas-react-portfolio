83a90356f54e1320564943c1f2c44e6b9f892b50
sshr0053 <sshr0053@yahoo.com>
2020-07-29 12:08:12 -0600

    worked on getting dropzone working

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import DropZoneComponent from 'react-dropzone-component';
+
+import RichTextEditor from "../forms/rich-text-editor";
 
 export default class BlogForm extends Component {
     constructor(props) {
@@ -7,11 +10,44 @@ export default class BlogForm extends Component {
 
         this.state = {
             title: "",
-            blog_status: ""
+            blog_status: "",
+            content: "",
+            featured_image: ""
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRichTextEditorChange = this.handleRichTextEditorChange.bind(this);
+        this.componentConfig = this.componentConfig.bind(this);
+        this.djsConfig = this.djsConfig.bind(this);
+        this.handleFeaturedImageDrop = this.handleFeaturedImageDrop.bind(this);
+
+        this.featuredImageRef = React.createRef();
+    }
+
+    componentConfig() {
+        return {
+            iconFiletypes:[".jpg", ".png"],
+            showFiletypeIcon: true,
+            postUrl: "https://httpbin.org/post"
+        }
+    }
+
+    djsConfig() {
+        return {
+            addRemoveLinks: true,
+            maxFiles: 1
+        }
+    }
+
+    handleFeaturedImageDrop() {
+        return {
+            addedfile: file => this.setState({ featured_image: file })
+        };
+    }
+
+    handleRichTextEditorChange(content) {
+        this.setState({ content });
     }
 
     buildForm() {
@@ -19,18 +55,29 @@ export default class BlogForm extends Component {
 
         formData.append("portfolio_blog[title]", this.state.title);
         formData.append("portfolio_blog[blog_status]", this.state.blog_status);
+        formData.append("portfolio_blog[content]", this.state.content);
+
+        if (this.state.featured_image) {
+            formData.append("portfolio_blog[featured_image]", this.state.featured_image);
+        }
 
         return formData;
     }
 
     handleSubmit(event) {
         axios.post("https://sabinshrestha.devcamp.space/portfolio/portfolio_blogs", this.buildForm(), { withCredentials: true }).then(response => {
-            this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
+            if (this.state.featured_image) {
+                this.featuredImageRef.current.dropzone.removeAllFiles()
+            }
 
             this.setState({
                 title:"",
-                blog_status: ""
+                blog_status: "",
+                content: "",
+                featured_image: ""
             });
+
+            this.props.handleSuccessfullFormSubmission(response.data.portfolio_blog);
         }).catch(error => {
             console.log("handleSubmit for blog error", error);
         });
@@ -63,6 +110,21 @@ export default class BlogForm extends Component {
                         placeholder="Blog Status"
                         value={this.state.blog_status}
                     />
+                 </div>
+
+                 <div className="one-column">
+                     <RichTextEditor handleRichTextEditorChange={this.handleRichTextEditorChange}/>
+                 </div>
+
+                 <div className="image-uploaders">
+                     <DropZoneComponent 
+                     ref={this.featuredImageRef}
+                     config={this.componentConfig()}
+                     djsConfig={this.djsConfig()}
+                     eventHandlers={this.handleFeaturedImageDrop()}
+                     >
+                         <div className="dz-message">Featured Image</div>
+                     </DropZoneComponent>
                  </div>
 
                 <button className="btn">Save</button>
---
+++ src/components/forms/rich-text-editor.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import { EditorState, convertToRaw } from "draft-js";
+import { Editor } from "react-draft-wysiwyg";
+import draftToHtml from "draftjs-to-html";
+import htmlToDraft from "html-to-draftjs";
+
+export default class RichTextEditor extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            editorState: EditorState.createEmpty()
+        };
+
+        this.onEditorStateChange = this.onEditorStateChange.bind(this);
+    }
+
+    onEditorStateChange(editorState) {
+        this.setState(
+            { editorState }, 
+            this.props.handleRichTextEditorChange(
+                draftToHtml(convertToRaw(this.state.editorState.getCurrentContent()))
+            )
+        );
+    }
+
+    uploadFile(file) {
+        console.log("upload file", file)
+    }
+
+    render() {
+        return (
+            <div>
+                <Editor 
+                editorState={this.state.editorState}
+                wrapperClassName="demo-wrapper"
+                editorClassName="demo-editor"
+                onEditorStateChange={this.onEditorStateChange}
+                toolbar={{
+                    inline: { inDropdown: true },
+                    list: { inDropdown: true },
+                    textAlign: { inDropdown: true },
+                    link: { inDropdown: true },
+                    history: { inDropdown: true },
+                    image: {
+                        uploadCallback: this.uploadFile,
+                        alt: { present: true, mandatory: false },
+                        previewImage: true,
+                        inputAccept: "image/gif,image/jpeg,image/jpg,image/png,image/svg"
+                    }
+                }}
+                />
+            </div>
+        );
+    }
+}
