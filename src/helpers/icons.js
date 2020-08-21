3628f49c84dd2f14c61668f4ed94a3d1342d76c6
sshr0053 <sshr0053@yahoo.com>
2020-08-21 15:15:42 -0600

    Finished project

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -6,13 +6,14 @@ import {
     faPlusCircle,
     faPhone,
     faEnvelope,
-    faMapMarkedAlt
+    faMapMarkedAlt,
+    faLock
 } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 const Icons = () => {
     return library.add(faTrash, faSignOutAlt, faEdit, faCircleNotch, faPlusCircle, faPhone,
-        faEnvelope,faMapMarkedAlt);
+        faEnvelope, faMapMarkedAlt, faLock);
 };
 
 export default Icons;
